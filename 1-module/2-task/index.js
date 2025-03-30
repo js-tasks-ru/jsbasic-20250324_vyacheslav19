eaa257d55964dfb44447f0bfb05ffdf0f4fe9aff
GoodFeelinge <vaceslavsobolev52536@gmail.com>
2025-03-30 21:08:33 +0700

    work1-solution

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,11 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  if (name!=null && name.length>=4 && name.includes(' ')!=true) {
-    return true;
-  } else {
-    return false;
-  }
+  return name!=null && name.length>=4 && !name.includes(' ');
 }
 
 function sayHello() {
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,7 +1,3 @@
 function checkSpam(str) {
-  if (str.toUpperCase().includes('1xBet'.toUpperCase()) || str.toUpperCase().includes('XXX')) {
-    return true
-   } else {
-    return false
-   }
+  return str.toUpperCase().includes('1xBet'.toUpperCase()) || str.toUpperCase().includes('XXX');
 }
