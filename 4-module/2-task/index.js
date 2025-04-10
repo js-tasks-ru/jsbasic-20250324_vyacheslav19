8147ea3e4bcf144ff00fdffc30bdaba9c866d392
GoodFeelinge <vaceslavsobolev52536@gmail.com>
2025-04-10 14:53:52 +0700

    work4-solution

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,7 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+for (let i = 0; i < table.rows.length; i++) {
+	if (table.rows[i].cells[i]) {
+            table.rows[i].cells[i].style.backgroundColor = 'red';
+	}
+}
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,17 @@
 function highlight(table) {
-  // ваш код...
+  const tbody = table.querySelector('tbody');
+  for (const row of tbody.rows) {
+    const isAvailable = row.cells[3].dataset.available;
+    const age = parseInt(row.cells[1].textContent.trim(), 10);
+    const gender = row.cells[2].textContent.trim();
+    if (isAvailable !== undefined) {
+      row.classList.add(isAvailable === 'true' ? 'available' : 'unavailable');
+    } else {
+      row.setAttribute('hidden', true);
+    }
+    row.classList.add(gender === 'm' ? 'male' : 'female');
+    if (age < 18) {
+      row.style.textDecoration = 'line-through'; 
+    }
+  }
 }
